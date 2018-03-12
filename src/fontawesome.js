9aad89479a27b8a113d7edf929c3da50af82d29e
Og Astorga <og@ogastorga.com>
2018-03-12 17:59:52 -0600

    add fontawesome

---
+++ src/fontawesome.js
@@ -0,0 +1,7 @@
+import fontawesome from '@fortawesome/fontawesome';
+import FontAwesomeIcon from '@fortawesome/vue-fontawesome';
+import solid from '@fortawesome/fontawesome-free-solid';
+
+fontawesome.library.add(solid); // Use any icon from the Solid style
+
+export default FontAwesomeIcon;
--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import Vue from 'vue';
 import App from './App';
 import router from './router';
 
+// set fontawesome available across app
+import FontAwesomeIcon from './fontawesome';
+
+Vue.component('icon', FontAwesomeIcon);
+
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
