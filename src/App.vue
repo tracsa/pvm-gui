a7d02c7b3efb81146fa6713ec349917aa33132a1
Og Astorga <og@ogastorga.com>
2018-03-12 13:08:47 -0600

    add bootstrap4 styles

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,10 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
+@import './styles/variables.scss';
+@import '~bootstrap/scss/bootstrap.scss';
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
