1f4bba7a65ab095bce5baf28b4b7001a467e06ba
Og Astorga <og@ogastorga.com>
2018-03-12 18:00:25 -0600

    set app background & font-family

--- src/App.vue
+++ src/App.vue
@@ -14,12 +14,14 @@ export default {
 @import './styles/variables.scss';
 @import '~bootstrap/scss/bootstrap.scss';
 
+html, body, #app {
+  height: 100vh;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  /* Linear gradient background */
+  background: -moz-linear-gradient(top, #f6f8f9 0%, #edf1f2 100%);
+  background: -webkit-linear-gradient(top, #f6f8f9 0%,#edf1f2 100%);
+  background: linear-gradient(to bottom, #f6f8f9 0%,#edf1f2 100%);
 }
 </style>
