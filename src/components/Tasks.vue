6b74f69f368478b88c02bc4f14ba5726d7402746
Og Astorga <og@ogastorga.com>
2018-03-13 17:16:52 -0600

    Improve tasks list

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -2,19 +2,33 @@
   <div
     class="tasks-container"
     v-bind:class="containerClass">
-    <h1>this is a title</h1>
+    <h1>&lt;header /&gt;</h1>
     <div
       class="row">
       <div class="col">
         <div class="card">
-          <ul class="list-group">
+          <div class="tasks-header">
+            <div class="tasks-sync">
+              <icon :icon="['fas', 'sync-alt']" />
+            </div>
+            My tasks
+          </div>
+          <ul class="tasks-list">
             <li
-              class="list-group-item"
+              class="task-item"
               v-bind:class="{ active: selected === task.id }"
               v-for="task in tasks"
               :key="task.id"
               v-on:click="openTask($event, task)">
-              {{ task.title }}
+              <div class="task-left">
+                {{ task.title }}
+              </div>
+              <div class="task-right">
+                <span class="task-elapsed">{{ task.elapsed }}</span>
+                <div class="task-details-icon">
+                  <icon :icon="['fas', 'caret-right']" />
+                </div>
+              </div>
             </li>
           </ul>
         </div>
@@ -49,7 +63,16 @@ export default {
       tasks: [
         {
           id: '1',
-          title: 'Generar contrato',
+          title: 'Generar contrato de arrendamiento',
+          elapsed: '1h 42m',
+          form: [
+            { label: '', name: '', value: '', type: '' },
+          ],
+        },
+        {
+          id: '2',
+          title: 'Aprovar salida de utilitario',
+          elapsed: '3m',
           form: [
             { label: '', name: '', value: '', type: '' },
           ],
@@ -93,12 +116,15 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+@import '../styles/variables.scss';
+
 .card {
   border-radius: 3px 3px 0 0;
   box-shadow: 0 1px 3px 0 rgba(0,0,0,0.15);
   border-color: #fff;
   flex: 1;
+  overflow: hidden;
 }
 
 .task {
@@ -125,5 +151,86 @@ export default {
 
 .col {
   display: flex;
+}
+
+.tasks-header {
+  border-bottom: 1px solid white;
+  box-shadow: 0 0 5px rgba(0,0,0,0.5);
+  padding: 10px 30px;
+  z-index: 1;
+
+  .tasks-sync {
+    float: right;
+    color: $purple;
+  }
+}
+
+.tasks-list {
+  display: flex;
+  flex-direction: column;
+
+  padding-left: 0;
+  margin-bottom: 0;
+}
+
+$task-list-height: 36px;
+
+.task-item {
+  font-size: 13px;
+
+  position: relative;
+  display: block;
+  padding: 0;
+  margin: 0 30px;
+  height: $task-list-height;
+  display: flex;
+
+  border-bottom: 1px solid $gray-300;
+
+  &.active {
+    padding: 0 30px;
+    margin: 0;
+    border-bottom: none;
+    background: $purple;
+    color: white;
+
+    .task-right {
+      .task-elapsed {
+        color: white;
+      }
+
+      .task-details-icon {
+        right: 0;
+        color: white;
+      }
+    }
+  }
+
+  .task-left, .task-right {
+    display: flex;
+    align-items: center;
+  }
+
+  .task-left {
+    flex-grow: 1;
+  }
+
+  .task-right {
+    .task-elapsed {
+      color: $gray-500;
+      font-size: 0.9em;
+    }
+
+    .task-details-icon {
+      cursor: pointer;
+      color: $gray-500;
+      position: absolute;
+      right: -30px;
+      height: $task-list-height;
+      width: 30px;
+      line-height: $task-list-height;
+      text-align: center;
+    }
+  }
 }
 </style>
