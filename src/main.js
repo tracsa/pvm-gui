b5d5b5f904299baf882912ed7266e4cdd981a133
Og Astorga <og@ogastorga.com>
2018-03-28 16:31:23 -0600

    fetch-polyfill

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import 'whatwg-fetch';
 import App from './App';
 import router from './router';
 import './fontawesome';
+
 
 Vue.config.productionTip = false;
 
