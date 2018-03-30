7ecc2e1a104a589bb94c923286463af377ed06e9
Og Astorga <og@ogastorga.com>
2018-03-30 12:30:45 -0600

    Wrong http code

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -35,7 +35,7 @@ export function login(username, password, callback) {
 
   window.fetch(authUri, params)
     .then((response) => {
-      if (response.status !== 400) {
+      if (response.status !== 200) {
         const error = new Error(response.statusText);
         error.response = response;
         throw error;
