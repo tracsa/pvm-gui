1d1660361ce004e55ca1c4bd7b533ecf3b5919db
Alex C <alex@debian>
2018-03-31 13:36:02 -0600

    Cambios

--- src/settings.js
+++ src/settings.js
@@ -1,13 +1,3 @@
-const settings = {
-  pvm: {
-    host: 'localhost',
-    port: 5000,
-    authProvider: 'ldap',
-  },
-  doquer: {
-    host: 'localhost',
-    port: 6000,
-  },
-};
+import settings from '../settings';
 
-module.exports = settings;
+export default settings;
