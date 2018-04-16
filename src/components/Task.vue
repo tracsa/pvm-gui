333223d2698679ebbe3f07553d1da2a0e3b1e6db
Ulises C <me@ulisescastro.com>
2018-04-16 13:07:32 -0500

    Fix form render, now it shows.

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,23 +1,24 @@
 <template>
   <div v-if="!loading">
-    <div class="timeline" v-for="form in task.form_array" :key="form.ref">
+    <div class="timeline">
       <div class="card timeline-action">
-          <div class="card-body">
-            <div class="float-right">
-              <router-link :to="{ path: '/tracking'}">
-                <icon :icon="['fas', 'times']" />
-              </router-link>
-            </div>
-            <div
-              v-for="error in errors"
-              :key="error.code"
-              class="alert custom-alert-danger">
-              {{ $t('error.code') }}
-            </div>
-            <form-render
-              :form="form"
-              @submit= "submit"
-            />
+        <div class="card-body">
+          <div class="float-right">
+            <router-link :to="{ path: '/tracking'}">
+              <icon :icon="['fas', 'times']" />
+            </router-link>
+          </div>
+          <div
+            v-for="error in errors"
+            :key="error.code"
+            class="alert custom-alert-danger">
+            {{ $t('error.code') }}
+          </div>
+
+          <form-render
+            :forms="task.form_array"
+            @submit= "submit"
+          />
         </div>
       </div>
     </div>
