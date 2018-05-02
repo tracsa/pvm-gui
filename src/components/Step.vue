9b34871b655bccf91cac8cab03add0fb40ce1e9c
Ulises C <me@ulisescastro.com>
2018-05-01 19:19:32 -0500

    Added step component.

---
+++ src/components/Step.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <div 
+          v-for="d in data"
+          class="card">
+          <div class="card-body">
+            <h4 class="card-title">
+              {{d.title}}
+            </h4>
+            <p class="card-text">
+              {{d.desc}}
+            </p>
+            <span class="card-time">
+               — {{ d.time }}
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  props: ['data']
+</script>
+<style lang="scss">
+@import '../styles/variables.scss';
+
+.card {
+  .card-body {
+    .card-time {
+      font-size: 12px;
+    }
+  }
+}
+
+</style>
