52559940a945657d1e2dd7a4c8fda4b91d48043e
ulisescastro2015@gmail.com <ulisescastro2015@gmail.com>
2018-05-16 22:42:43 -0500

    Set momento to spanish

--- src/main.js
+++ src/main.js
@@ -1,12 +1,9 @@
 import Vue from 'vue';
-import moment from 'moment';
 import 'whatwg-fetch';
 import App from './App';
 import router from './router';
 import './fontawesome';
 import i18n from './i18n';
-
-moment.locale('es');
 
 Vue.config.productionTip = false;
 
