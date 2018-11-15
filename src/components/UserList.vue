335aaa2c49a35a4ee2415e9d3e99b3dcf236273c
KYSXD <dracojan_12@hotmail.com>
2018-11-15 17:05:53 -0600

    Added first iteration of UserList
    a.k.a. admin list item - users list

---
+++ src/components/UserList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="header-container">
+    <div
+      v-for="(user, index) in users"
+      :key="index">
+      {{ user.fullname }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['users'],
+  data() {
+    return {
+      sleep: 0,
+      tiemoutId: 0,
+    };
+  },
+  mounted() {
+  },
+};
+</script>
+
+<style>
+</style>
