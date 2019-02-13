7f2d040d59f132281fbb920d1545d0b91d909bc4
cnpoe <cnpoek@gmail.com>
2019-02-13 14:20:09 -0600

    url updated

--- src/components/History.vue
+++ src/components/History.vue
@@ -175,7 +175,7 @@ export default {
       this.loadingList = true;
       this.errors = [];
 
-      get(`/inbox?actor_identifier=${this.userId}&status=finished`)
+      get(`/inbox?actor_identifier=${this.userIdentifier}&status=finished`)
         .then((body) => {
           this.loadingList = false;
           this.items = body.data;
