03e95eea9e70cb248d53beaecd48bb4c44f80b29
Og Astorga <og@ogastorga.com>
2019-02-18 09:13:20 -0600

    patch

--- src/components/timeline/index.js
+++ src/components/timeline/index.js
@@ -4,8 +4,10 @@ import Action from './Action';
 import Validation from './Validation';
 import Pending from './Pending';
 import Task from './Task';
+import Patch from './Patch';
 
 Vue.component('timeline-action', Action);
 Vue.component('timeline-validation', Validation);
 Vue.component('timeline-pending', Pending);
 Vue.component('timeline-task', Task);
+Vue.component('timeline-patch', Patch);
