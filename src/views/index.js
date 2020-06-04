a51f5fb220186066df25d10351b43f2ecaf7b5b7
Kevin Villalobos <dracojan_12@hotmail.com>
2020-06-04 17:41:20 -0500

    Added views.index

---
+++ src/views/index.js
@@ -0,0 +1,7 @@
+import Vue from 'vue';
+
+import History from './History';
+import UserProfile from './UserProfile';
+
+Vue.component('ca-history', History);
+Vue.component('ca-user-profile', UserProfile);
