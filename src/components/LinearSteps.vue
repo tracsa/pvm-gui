b06a2c1b63aca2a0158b20832196822161186e55
Kevin Villalobos <dracojan_12@hotmail.com>
2020-11-18 11:55:50 -0600

    Added missing data in component

--- src/components/LinearSteps.vue
+++ src/components/LinearSteps.vue
@@ -86,6 +86,12 @@ export default {
     selected: String,
   },
 
+  data() {
+    return {
+      uuid: Math.random(),
+    };
+  },
+
   computed: {
     actualNodes() {
       const index = this.nodes
