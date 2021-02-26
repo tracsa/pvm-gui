2454025d0fa231d813f62945f123aa8a9b7c1e65
Kevin Villalobos <dracojan_12@hotmail.com>
2021-02-25 23:33:39 -0600

    delete unused tag

--- src/components/timeline/Action.vue
+++ src/components/timeline/Action.vue
@@ -23,49 +23,45 @@
                       <span>Mostrar campos vacios</span>
                     </b-form-checkbox>
 
-                    <span
-                      v-if="listForms.length"
+                    <div
+                      class="border-left border-primary pl-2 mb-3"
+                      v-for="(form,fIndex) in listForms"
+                      v-bind:key="fIndex"
                     >
-                      <div
-                        class="border-left border-primary pl-2 mb-3"
-                        v-for="(form,fIndex) in listForms"
-                        v-bind:key="fIndex"
-                      >
-                        <div>
-                          <router-link
-                            :to="{
-                              name: 'dashboard',
-                              params: { identifier: form.actor.identifier },
-                            }"
-                          >
-                            <icon :icon="['fa', 'user']" class="mr-1"/>
-                            <b>{{ form.actor.fullname }}</b>
-                          </router-link>
-                          <span> llenó la siguiente información:</span>
-                        </div>
-
-                        <div
-                          class="border-left border-secondary pl-2 mb-3"
-                          v-for="(input,iIndex) in listInputs(form.inputs)"
-                          v-bind:key="iIndex"
+                      <div>
+                        <router-link
+                          :to="{
+                            name: 'dashboard',
+                            params: { identifier: form.actor.identifier },
+                          }"
                         >
-                          <small
-                            :class="{ 'text-muted': isEmptyField(input)}"
-                          >{{ input.label }}</small><br/>
-
-                          <p
-                            :class="{ 'text-muted': isEmptyField(input) }"
-                          ><value-render :input="input"/><br/></p>
-                        </div>
+                          <icon :icon="['fa', 'user']" class="mr-1"/>
+                          <b>{{ form.actor.fullname }}</b>
+                        </router-link>
+                        <span> llenó la siguiente información:</span>
                       </div>
-                    </span>
 
+                      <div
+                        class="border-left border-secondary pl-2 mb-3"
+                        v-for="(input,iIndex) in listInputs(form.inputs)"
+                        v-bind:key="iIndex"
+                      >
+                        <small
+                          :class="{ 'text-muted': isEmptyField(input)}"
+                        >{{ input.label }}</small><br/>
 
-                    <span v-else>
-                      <div class="text-center mb-3">
-                        <span>No hay información para mostrar</span>
+                        <p
+                          :class="{ 'text-muted': isEmptyField(input) }"
+                        ><value-render :input="input"/><br/></p>
                       </div>
-                    </span>
+                    </div>
+
+                    <div
+                      class="text-center mb-3"
+                      v-if="!listForms.length"
+                    >
+                      <span>No hay información para mostrar</span>
+                    </div>
                   </div>
                 </div>
               </div>
