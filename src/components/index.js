881488465423fe20a77741ba13ec99cdccc76efa
Kevin Villalobos <dracojan_12@hotmail.com>
2021-03-10 00:20:59 -0600

    Remove unused imports

--- src/components/index.js
+++ src/components/index.js
@@ -19,19 +19,8 @@ import Hero from './Hero';
 import Processes from './Processes';
 import SignIn from './SignIn';
 import LinkInput from './LinkInput';
-import TaskRedirect from './TaskRedirect';
-import Inbox from './Inbox';
-import InboxListItem from './InboxListItem';
-import InboxItem from './InboxItem';
 
 import BaseLayout from './BaseLayout';
-
-import UserProfileUserInfo from './userProfile/UserProfileUserInfo';
-
-import UserProfileItem from './userProfile/UserProfileItem';
-import UserProfileItemSummary from './userProfile/userProfileItem/UserProfileItemSummary';
-import UserProfileItemFinishedDetail from './userProfile/userProfileItem/UserProfileItemFinishedDetail';
-import UserProfileItemCancelledDetail from './userProfile/userProfileItem/UserProfileItemCancelledDetail';
 
 import ExecutionCard from './ExecutionCard';
 
@@ -47,11 +36,6 @@ Vue.component('app-inbox-execution-timeline', InboxExecutionTimeline);
 
 Vue.component('app-execution-card', ExecutionCard);
 
-// Inbox
-Vue.component('inbox', Inbox);
-Vue.component('inbox-list-item', InboxListItem);
-Vue.component('inbox-item', InboxItem);
-
 // Components
 Vue.component('app', App);
 Vue.component('app-header', Header);
@@ -64,12 +48,6 @@ Vue.component('processes', Processes);
 Vue.component('sign-in', SignIn);
 
 Vue.component('base-layout', BaseLayout);
-Vue.component('ca-user-profile-user-info', UserProfileUserInfo);
-
-Vue.component('user-profile-item', UserProfileItem);
-Vue.component('ca-user-profile-item-summary', UserProfileItemSummary);
-Vue.component('ca-user-profile-item-finished-detail', UserProfileItemFinishedDetail);
-Vue.component('ca-user-profile-item-cancelled-detail', UserProfileItemCancelledDetail);
 
 // From inputs
 Vue.component('form-instance', FormInstance);
@@ -85,5 +63,3 @@ Vue.component('doqer-suggest', DoqerSuggest);
 
 // Data validator
 Vue.component('data-validator', DataValidator);
-
-Vue.component('task-redirect', TaskRedirect);
