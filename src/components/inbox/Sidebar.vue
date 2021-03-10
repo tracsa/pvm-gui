afe31e17a921cccd68524f25220dfb9f15b53051
Kevin Villalobos <dracojan_12@hotmail.com>
2021-03-09 22:51:01 -0600

    sidebar with only links

--- src/components/inbox/Sidebar.vue
+++ src/components/inbox/Sidebar.vue
@@ -6,111 +6,51 @@
       pills
     >
       <b-nav-item
-        v-for="(item, index) in listOptions"
+        v-for="(item, index) in searchOptions"
         :key="index"
-        :active="item.value === selectedList"
-        @click="selectedList = item.value; selectOption(item.value)"
+        :to="{
+          name: 'dashboard',
+          query: { feed: item.value },
+        }"
+        :active="item.value === selectedSearch"
       >{{ item.label }}</b-nav-item>
     </b-nav>
   </div>
 </template>
 
 <script>
-import { getAuthUser } from '../../utils/auth';
-
 export default {
+  props: ['selectedSearch'],
+
   data() {
     return {
-      selectedList: null,
-      listOptions: [
+      searchOptions: [
         {
           value: 'myPendingTasks',
           label: 'Mis tareas pendientes',
-          description: 'Aquí podrás ver las tareas que faltan por realizar',
         },
         {
           value: 'myTasks',
           label: 'Tareas relacionadas conmigo',
-          description: 'Estas son todas las tareas que te asignaron o realizaste',
         },
         {
           value: 'allOngoingTasks',
           label: 'Todas las tareas pendientes',
-          description: 'Estás viendo todas las tareas en curso, de todos los usuarios',
         },
         {
           value: 'allOngoingExecutions',
           label: 'Todos los flujos de autorización en curso',
-          description: '¿Quieres ver todos los procesos en curso? Aquí estan',
         },
         {
           value: 'executionHistory',
           label: 'Historal de flujos de autorización',
-          description: '¿Buscas un proceso finalizado o cancelado? Este es el lugar correcto',
         },
         {
           value: 'taskHistory',
           label: 'Historal de tareas',
-          description: '¿En busca de tareas finalizadas o canceladas? Llegaste al lugar indicado',
         },
       ],
     };
-  },
-
-  methods: {
-    selectOption(opt) {
-      const vm = this;
-
-      const searchParams = {
-        ...this.listOptions.find(x => x.value === opt),
-      };
-
-      if (opt === 'myPendingTasks') {
-        const user = getAuthUser();
-
-        searchParams.objType = 'pointer';
-        searchParams.payload = {
-          executionStatus: ['ongoing'],
-          pointerStatus: ['ongoing'],
-          notifiedUsers: [user.username],
-        };
-      } else if (opt === 'myTasks') {
-        const user = getAuthUser();
-
-        searchParams.objType = 'pointer';
-        searchParams.payload = {
-          actoredUsers: [user.username],
-          notifiedUsers: [user.username],
-        };
-      } else if (opt === 'allOngoingTasks') {
-        searchParams.objType = 'pointer';
-        searchParams.payload = {
-          executionStatus: ['ongoing'],
-          pointerStatus: ['ongoing'],
-        };
-      } else if (opt === 'allOngoingExecutions') {
-        searchParams.objType = 'execution';
-        searchParams.payload = {
-          executionStatus: ['ongoing'],
-        };
-      } else if (opt === 'executionHistory') {
-        searchParams.objType = 'execution';
-        searchParams.payload = {
-          executionStatus: ['finished', 'cancelled'],
-        };
-      } else if (opt === 'taskHistory') {
-        searchParams.objType = 'pointer';
-        searchParams.payload = {
-          pointerStatus: ['finished', 'cancelled'],
-          onlyUserAndPatch: true,
-        };
-      }
-
-      vm.$emit(
-        'select-search',
-        searchParams,
-      );
-    },
   },
 };
 </script>
